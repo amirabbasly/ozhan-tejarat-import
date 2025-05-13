2f743b0bd6089e48a54a4851c1d03b67b926cf74
unknown <4liaghaie@gmail.com>
2025-05-13 04:10:59 -0700

    new server deploy8

--- core/settings.py
+++ core/settings.py
@@ -275,7 +275,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend-static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = env("STATIC_ROOT", "/static")      # volume mounted in Compose
 MEDIA_ROOT = env("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))

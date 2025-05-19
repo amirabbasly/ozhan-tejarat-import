8633c885963dd9bf391eab91e8886207fbd91edc
unknown <4liaghaie@gmail.com>
2025-05-19 04:42:23 -0700

    server 19may5

--- core/settings.py
+++ core/settings.py
@@ -275,8 +275,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/backend-static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = "/backend-static/"
 STATIC_ROOT =  "/backend-static/"
 MEDIA_ROOT = env("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
 MEDIA_URL = "/media/"

ae0b77c4e210477ff769c2a599db0fe7dd8abb1f
unknown <4liaghaie@gmail.com>
2025-05-24 02:13:11 -0700

    server may 24th3

--- accounts/models.py
+++ accounts/models.py
@@ -52,6 +52,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class Costumer(models.Model):
     full_name = models.CharField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=100,null=True, blank=True, unique=True)
-    customer_birthday = models.CharField(max_length=100,null=True, blank=True, unique=True)
+    customer_birthday = models.CharField(max_length=100,null=True, blank=True)
     national_code = models.CharField(max_length=100, null=True, blank=True, unique=True)
     customer_address = models.TextField(null=True, blank=True)
